c28f2b3a74e0cef28c74cdd96287b4bf78fafcb0
jj289336@gmail.com <jj289336@gmail.com>
2024-06-07 14:59:14 +0900

    message

--- lab8/21_final/5.c
+++ lab8/21_final/5.c
@@ -4,7 +4,7 @@ int main(void) {
 	long a;
 	long b;
 	scanf("%ld %ld", &a,&b);
-	printf("1st integer %ld\n2nd integer %ld", a,b);
+	printf("1st integer %ld\n2nd integer %ld\n %ld", a,b,a+b);
 	return 0;
 }
 
